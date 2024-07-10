e042ef899087b26102927fd207b5e91f023ec315
eduardo paim silva <edups.ww.com@hotmail.com>
2024-07-09 21:16:27 -0300

    criação de viagem

---
+++ src/main/java/br/com/rocketseat/planner/trip/TripController.java
@@ -0,0 +1,30 @@
+package br.com.rocketseat.planner.trip;
+
+import br.com.rocketseat.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/trips")
+public class TripController {
+
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository repository;
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse>createTrip(@RequestBody TripRequestPayload payload){
+        Trip newTrip = new Trip(payload);
+
+        this.repository.save(newTrip);
+
+
+        this.participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
+
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+}
