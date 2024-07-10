4450fe5d4a1f3f5d1720a159d93328248d911845
eduardo paim silva <edups.ww.com@hotmail.com>
2024-07-10 00:13:23 -0300

    criando entidade que representa um participant

---
+++ src/main/java/br/com/rocketseat/planner/participant/Participant.java
@@ -0,0 +1,36 @@
+package br.com.rocketseat.planner.participant;
+
+import br.com.rocketseat.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(name = "name", nullable = false)
+    private String Name;
+
+    @Column(name = "email", nullable = false)
+    private String Email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
